aec5f402583afad5ded04698237cdc3017e87bbb
Andrej Pregl <andrej.pregl@icloud.com>
2018-11-09 11:40:17 +0100

    Automated reference link seding

--- gen.sh
+++ gen.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 resume export index.html --format html --theme kendall resume.json
+
+sed -i -e '0,/href=\"\"/s//href=\"https:\/\/se.linkedin.com\/in\/ramzif\"/' index.html
+sed -i -e '0,/href=\"\"/s//href=\"https:\/\/nl.linkedin.com\/in\/edslijkerman\"/' index.html
